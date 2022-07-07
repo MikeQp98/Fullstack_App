55fe588a58a7b3769858133a939e0540240f8bdc
Michael Peters <michaelrespond99@gmail.com>
2022-07-07 07:13:23 -0400

    Main day 2

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,20 @@
 import  express  from "express";
+import  dotenv  from   "dotenv"
+import conectarDB from "./config/db.js";
+
+const app = express();
+dotenv.config();
+
+conectarDB();
 
 
-const app = express()
 
-app.listen(4000, () => {
+app.use ("/", (req, res) => {
+    res.send('Hola Mundo')
+})
 
-    console.log('Servidor Funcionando en el puerto 4000')
+const PORT = process.env.PORT || 4000
+
+app.listen( PORT, () => {
+    console.log(`Servidor funcionando en el puerto {PORT}`)
 })
